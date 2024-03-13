2ef59d52aa8966be812181a24036d3d12eecae3f
Terry <master_correo144@hotmail.com>
2024-03-12 22:44:57 -0500

    INSTALL AND ADD Canvas

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,16 @@
+import { Canvas } from "@react-three/fiber";
+
+{
+  /* <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
+  POPUP
+</div> */
+}
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas></Canvas>
+    </section>
+  );
 };
 
 export default Home;
