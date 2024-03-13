2de91e70a3dbce5dc687b237c88641a614956084
Terry <master_correo144@hotmail.com>
2024-03-12 23:03:52 -0500

    ADD loader with drei

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,6 @@
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/loader";
 
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
@@ -8,7 +10,12 @@ import { Canvas } from "@react-three/fiber";
 const Home = () => {
   return (
     <section className="w-full h-screen relative">
-      <Canvas></Canvas>
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />} />
+      </Canvas>
     </section>
   );
 };
